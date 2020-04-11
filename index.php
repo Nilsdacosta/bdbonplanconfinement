7596ada16111f5e2f60a66287d8ae0aec31bed77
Nilsdacosta <nilsdacosta@gmail.com>
2020-04-11 17:17:13 +0200

    changement favicon

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
-        <link rel="shortcut icon" href="image/50-ans-dédition-Glénat_couv.jpg" type="image/x-icon">
+        <link rel="shortcut icon" href="image/bdbulle.png" type="image/x-icon">
         <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
 
     </head>
